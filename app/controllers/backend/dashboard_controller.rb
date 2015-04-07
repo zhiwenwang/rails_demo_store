b9a04b4f6f93fdcea187e063c6eee753d247174a
Eddie Kao <eddie@digik.com.tw>
2015-04-07 20:33:18 +0800

    add backend and dashboard page

---
+++ app/controllers/backend/dashboard_controller.rb
@@ -0,0 +1,8 @@
+class Backend::DashboardController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
+
+  namespace :backend do
+    get 'dashboard', to:'dashboard#index'
+  end
 end
