b6fc8e5d0a8856e7a3a49d3262f6c67d5fc75aa3
Eddie Kao <eddie@digik.com.tw>
2015-04-07 21:23:04 +0800

    add backend/base controller

--- app/controllers/backend/dashboard_controller.rb
+++ app/controllers/backend/dashboard_controller.rb
@@ -1,13 +1,4 @@
-class Backend::DashboardController < ApplicationController
-
-  layout 'backend'
-
-  before_action :authenticate_user!
-
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
-  end
-
+class Backend::DashboardController < Backend::BaseController
   def index
     authorize! :manage, :all
   end
